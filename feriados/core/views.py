98730c189bf10b988667b30a58c1c8300565bd65
brunounky <bruno_unky@hotmail.com>
2022-04-13 20:27:50 -0300

    Update README.md

---
+++ feriados/core/views.py
@@ -0,0 +1,21 @@
+from multiprocessing import context
+from django.shortcuts import render
+from datetime import date
+
+def natal(request):
+    hoje = date.today()
+    if hoje.day == 7 and hoje.month == 4:
+        natal = True
+    else:
+        natal = False
+    contexto = {
+        'natal':natal,
+        'carnaval':False
+    }
+    return render(request, 'natal.html', contexto)
+
+def tiradentes(request):
+    context={
+        'tiradentes': True,
+    }
+    return render(request, 'tiradentes.html', context)
