213e4a172f94a996f19837cff426e4f87703d391
brunounky <bruno_unky@hotmail.com>
2022-10-21 23:16:26 -0300

    Update README.md

--- feriados/core/models.py
+++ feriados/core/models.py
@@ -1,3 +1,13 @@
+from operator import mod
+from tabnanny import verbose
 from django.db import models
 
-# Create your models here.
+class FeriadoModel(models.Model):
+    nome = models.CharField('Feriado', max_length=50)
+    dia = models.IntegerField()
+    mes = models.IntegerField()
+
+modificado_em = models.DateTimeField(verbose_name='modificado em', auto_now_add=False, auto_now=True)
+
+def __str__(self):
+ return self.nome
